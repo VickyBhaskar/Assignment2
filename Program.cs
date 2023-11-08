e18e1b61e0db9b4a6057ebe52683aeaffce2c3e3
VickyBhaskar <vicky2400kumar@gmail.com>
2023-11-08 15:05:28 +0530

    initial Commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(String[]args)
+        {
+            Console.WriteLine("Enter Employee ID: ");
+            int employeeId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee Name: ");
+            string employeeName = Console.ReadLine();
+
+            Console.WriteLine("Enter Employee Gender (M/F): ");
+            char employeeGender = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Employee Salary: ");
+            double employeeSalary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Date of Joining (DD/MM/YYYY): ");
+            DateTime dateOfJoining = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+
+            double taxRate;
+            double taxAmount;
+
+            if (employeeSalary > 90000)
+            {
+                 taxRate = 0.30;
+            }
+            else
+            {
+                 taxRate = 0.15;
+            }
+            taxAmount = employeeSalary * taxRate;
+
+            Console.WriteLine("\nID\tEmployee Name\tEmployee Gender\tEmployee Salary\tDOJ");
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine($"{employeeId}\t{employeeName}\t\t{employeeGender}\t\t{employeeSalary:C}\t{dateOfJoining:dd/MM/yyyy}");
+            Console.WriteLine("\nYou have to pay: " + taxAmount.ToString("C"));
+        }
+    }
+}
+
